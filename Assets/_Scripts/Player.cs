a1c559ca401f1de09d06740b7b0687fad0ba1138
Yeray <ytarifa2001@gmail.com>
2022-10-06 15:03:44 +0200

    Update Player.cs

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     private void OnEnable()
     {
-        Simulator.OnNewPlayer += Pla;
+        Simulator.OnNewPlayer += PlayerInstall;
+        Simulator.OnNewSession += PlayerStartSession;
+        Simulator.OnEndSession += PlayerEndSession;
+        Simulator.OnBuyItem += PlayerTransaction;
     }
 
-    private void Pla(string arg1, string arg2, DateTime arg3)
+    private void PlayerInstall(string arg1, string arg2, DateTime arg3)
+    {
+
+        throw new NotImplementedException();
+    }
+
+    private void PlayerStartSession(DateTime obj)
     {
         throw new NotImplementedException();
     }
 
-    // Start is called before the first frame update
-    void Start()
+    private void PlayerEndSession(DateTime obj)
     {
-        
+        throw new NotImplementedException();
+    }
+    
+    private void PlayerTransaction(int arg1, DateTime arg2)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public string url = "https://citmalumnes.upc.es/~yeraytm/hello.php";
+    IEnumerator SendUserInstall(string arg1, string arg2, DateTime arg3)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", arg1);
+        form.AddField("country", arg2);
+        form.AddField("dateInstall", arg3.ToString());
+
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //void Update()
+    //{
+    //}
 }
